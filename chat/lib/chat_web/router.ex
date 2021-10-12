51adf9cc051f7a55e5a9995fde9de3f6180b1460
Rohde Fischer <rff@trifork.com>
2021-10-12 17:15:45 +0200

    Removed browser

--- chat/lib/chat_web/router.ex
+++ chat/lib/chat_web/router.ex
@@ -3,21 +3,12 @@ defmodule ChatWeb.Router do
   use Phoenix.Router
   import Phoenix.LiveView.Router
 
-  pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_live_flash
-    plug :put_root_layout, {ChatWeb.LayoutView, :root}
-    # plug :protect_from_forgery # disable CSRF
-    plug :put_secure_browser_headers
-  end
-
   pipeline :api do
     plug :accepts, ["json"]
   end
 
   scope "/", ChatWeb do
-    pipe_through :browser
+    pipe_through :api
     live "/chat", ChatLive, :index
     live "/room:name", ChatroomLive, :index
 
