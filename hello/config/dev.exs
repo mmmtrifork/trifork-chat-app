6608ac061fb3434202d90ddd53c12ad19242089f
Rohde Fischer <rff@trifork.com>
2021-10-12 09:59:18 +0200

    Database setup

--- hello/config/dev.exs
+++ hello/config/dev.exs
@@ -2,9 +2,9 @@ import Config
 
 # Configure your database
 config :hello, Hello.Repo,
-  username: "postgres",
-  password: "postgres",
-  database: "hello_dev",
+  username: "chat",
+  password: "password",
+  database: "chat",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
